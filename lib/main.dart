2a4953f46e536d4251bca219db903c034fd1dba4
basakertugrul <61563492+basakertugrul@users.noreply.github.com>
2020-10-05 03:37:07 +0300

    aciklama

---
+++ lib/main.dart
@@ -0,0 +1,244 @@
+import 'dart:async';
+
+import 'package:animated_bottom_navigation_bar/animated_bottom_navigation_bar.dart';
+import 'package:circular_reveal_animation/circular_reveal_animation.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        scaffoldBackgroundColor: Colors.white,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: MyHomePage(title: 'Animated Navigation Bottom Bar'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage>
+    with SingleTickerProviderStateMixin {
+  var _bottomNavIndex = 0; //default index of first screen
+//GİRİŞ SAYFASINI BURADAN SEÇİYORUZ
+  AnimationController _animationController;
+  Animation<double> animation;
+  CurvedAnimation curve;
+
+  final sentence = <String>["START WORKING", "25"];
+  int i = 0;
+
+  final iconList = <IconData>[
+    Icons.format_list_bulleted_rounded,
+    Icons.military_tech,
+    Icons.group,
+    Icons.person,
+  ];
+
+  @override
+  void initState() {
+    super.initState();
+    final systemTheme = SystemUiOverlayStyle.light.copyWith(
+      systemNavigationBarColor: HexColor('#B4C3CC'),
+      systemNavigationBarIconBrightness: Brightness.light,
+    );
+    SystemChrome.setSystemUIOverlayStyle(systemTheme);
+
+    _animationController = AnimationController(
+      duration: Duration(seconds: 1),
+      vsync: this,
+    );
+    curve = CurvedAnimation(
+      parent: _animationController,
+      curve: Interval(
+        0.5,
+        1.0,
+        curve: Curves.fastOutSlowIn,
+      ),
+    );
+    animation = Tween<double>(
+      begin: 0,
+      end: 1,
+    ).animate(curve);
+
+    Future.delayed(
+      Duration(seconds: 1),
+      () => _animationController.forward(),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      extendBody: true,
+      appBar: AppBar(
+        title: Text(
+          widget.title,
+          style: TextStyle(color: Colors.white),
+        ),
+        backgroundColor: HexColor('#B4C3CC'),
+      ),
+      body:
+          /*NavigationScreen(
+       iconList[_bottomNavIndex],
+      
+      ),)*/
+          Container(
+              decoration: BoxDecoration(
+                  gradient: LinearGradient(
+                      begin: Alignment.topLeft,
+                      end: Alignment.bottomRight,
+                      colors: [
+                    HexColor('#B4C3CC'),
+                    HexColor('#ffffff'),
+                  ])),
+              child: Center(child: Text("Page" + _bottomNavIndex.toString()))),
+      floatingActionButton: ScaleTransition(
+        scale: animation,
+        child: FloatingActionButton(
+          elevation: 8,
+          backgroundColor: HexColor('#CCBDB4'),
+          child: Text(
+            sentence[i],
+            style: TextStyle(
+              fontWeight: FontWeight.bold,
+              color: Colors.black,
+              fontSize: 9.4,
+            ),
+            textAlign: TextAlign.center,
+          ),
+          onPressed: () {
+            setState(() {
+              i++;
+              i = i % 2;
+            });
+            _animationController.reset();
+            _animationController.forward();
+            //ZAMAN AYARI YAPILAN SAYFA AÇILIR SONRA OKEYE BASILINCA ANİMATİONA DEVAM EDİLEBİLİR
+          },
+        ),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      bottomNavigationBar: AnimatedBottomNavigationBar(
+        icons: iconList,
+        iconSize: 30.0,
+        backgroundColor: HexColor('#B4C3CC'),
+        activeIndex: _bottomNavIndex,
+        activeColor: HexColor('#1A445D'),
+        splashColor: HexColor('#1A445D'),
+        inactiveColor: Colors.white,
+        notchAndCornersAnimation: animation,
+        splashSpeedInMilliseconds: 300,
+        notchSmoothness: NotchSmoothness.defaultEdge,
+        gapLocation: GapLocation.center,
+        leftCornerRadius: 32,
+        rightCornerRadius: 32,
+        onTap: (index) => setState(() => _bottomNavIndex = index),
+      ),
+    );
+  }
+}
+
+class NavigationScreen extends StatefulWidget {
+  final IconData iconData;
+
+  NavigationScreen(this.iconData) : super();
+
+  @override
+  _NavigationScreenState createState() => _NavigationScreenState();
+}
+
+class _NavigationScreenState extends State<NavigationScreen>
+    with TickerProviderStateMixin {
+  AnimationController _controller;
+  Animation<double> animation;
+
+  @override
+  void didUpdateWidget(NavigationScreen oldWidget) {
+    super.didUpdateWidget(oldWidget);
+    if (oldWidget.iconData != widget.iconData) {
+      _startAnimation();
+    }
+  }
+
+  @override
+  void initState() {
+    _controller = AnimationController(
+      vsync: this,
+      duration: Duration(milliseconds: 1000),
+    );
+    animation = CurvedAnimation(
+      parent: _controller,
+      curve: Curves.easeIn,
+    );
+    _controller.forward();
+    super.initState();
+  }
+
+  _startAnimation() {
+    _controller = AnimationController(
+      vsync: this,
+      duration: Duration(milliseconds: 1000),
+    );
+    animation = CurvedAnimation(
+      parent: _controller,
+      curve: Curves.easeIn,
+    );
+    _controller.forward();
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      height: double.infinity,
+      color: Colors.white,
+      child: Center(
+        child: CircularRevealAnimation(
+          animation: animation,
+          centerOffset: Offset(80, 80),
+          maxRadius: MediaQuery.of(context).size.longestSide * 1.1,
+          child: Icon(
+            widget.iconData,
+            color: HexColor('#FFA400'),
+            size: 160,
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class HexColor extends Color {
+  HexColor(final String hexColor) : super(_getColorFromHex(hexColor));
+
+  static int _getColorFromHex(String hexColor) {
+    hexColor = hexColor.toUpperCase().replaceAll('#', '');
+    if (hexColor.length == 6) {
+      hexColor = 'FF' + hexColor;
+    }
+    return int.parse(hexColor, radix: 16);
+  }
+}
